1b875aee34e6087d254608554cb41a7bfe94414c
abcd-56 <116738471+abcd-56@users.noreply.github.com>
2023-07-18 18:48:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+song1 = "";
+song2 = "";
+song3 = "";
+song4 = "";
+song5 = "";
+song6 = "";
+
+function preload()
+{
+    song1 = loadSound("music2.mp3");
+    song2 = loadSound("music.mp3");
+    song3 = loadSound("Spectre.mp3");
+    song4 = loadSound("Symbolism.mp3");
+    song5 = loadSound("Mortals.mp3");
+    song6 = loadSound("Fade.mp3");
+}
+function play1()
+{
+    song1.play();
+    song1.setVolume(7);
+}
+function play2()
+{
+    song2.play();
+    song2.setVolume(7);
+}
+function play3()
+{
+    song3.play();
+    song3.setVolume(7);
+}
+function play4()
+{
+    song4.play();
+    song4.setVolume(7);
+}
+function play5()
+{
+    song5.play();
+    song5.setVolume(7);
+}
+function play6()
+{
+    song6.play();
+    song6.setVolume(7);
+}
